50b97691438aa16f5ee286b2efc450145b20c5f0
razorloveskeke <ataberkzxc@gmail.com>
2023-01-21 14:39:56 +0300

    full rework

--- game.js
+++ game.js
@@ -1,69 +1,210 @@
 const canvas = document.getElementById('game');
 
-// Fullscreen
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
-const tileSize = 32;
-let level = [
-	{ n: 'box', p: { x: 4, y: 4 } },
-	{ n: 'box', p: { x: 8, y: 4 } }
+const size = 40;
+const dirChar = [];
+dirChar[-1] = '-';
+dirChar[0] = '0',
+dirChar[+1] = '+';
+
+const levels = [
+`######
+#    #
+# #a #
+# bB #
+# GB #
+#    #
+######`,
+
+`  #####
+###   #
+# b # ##
+# #  G #
+#    # #
+## #   #
+ #a  ###
+ #####
+`,
+
+`####  
+# G#  
+#  ###
+#Ba  #
+#  b #
+#  ###
+####  `,
 ]
-let player = { p: { x: 0, y: 0 }, d: { x: 0, y: 0 } };
 
-function drawTile({ x: x, y: y }, c) {
-	ctx.fillStyle = c;
-	ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize)
-}
+let level;
+let goals;
+let player;
+let currentLevel = 0;
 
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawTile(player.p, 'blue');
-	for (let [i, tile] of level.entries()) {
-		switch (tile.n) {
-			case 'box':
-				drawTile(tile.p, 'goldenrod');
-				break;
+const alert = document.getElementById('alert');
+const levelSelect = document.getElementById('levels');
+
+function loadLevel(index) {
+	level = [];
+	goals = [];
+	player = [];
+	let rows = levels[index]
+		.split('\n')
+		.map(x => x.split(''));
+	for (let [y, row] of rows.entries()) {
+		for (let [x, char] of row.entries()) {
+			let pos = { x: x, y: y };
+			switch (char.toLowerCase()) {
+				// (n)ame, (p)osition, (d)irection
+				case ' ':
+					continue;
+				case '#':
+					level.push({ n: 'wall', p: pos });
+					continue;
+				case 'b':
+					level.push({ n: 'box', p: pos });
+					break;
+				case 'a':
+					player = { p: pos, d: { x: +1, y: 0 } };
+					break;
+			}
+			if (char === char.toUpperCase())
+				goals.push({ x: x, y: y });
 		}
-	}
-}
-
-function update(x, y) {
-	player.d.x = x; player.d.y = y;
-
-	let pushFail = false;
-	for (let [i, tile] of level.entries()) {
-		switch (tile.n) {
-			case 'box':
-				if (
-					(player.p.x + x) === tile.p.x &&
-					(player.p.y + y) === tile.p.y
-				) {
-					console.log('pushed box');
-					console.log(tile)
-					for (let [i2, tile2] of level.entries()) {
-						console.log(tile2);
-						if (
-							(tile2.n === 'wall' || tile2.n === 'box') &&
-							tile2.p === tile.p && i2 !== i
-						) {
-							console.log(i2);
-							console.log('nvm lmfao');
-							pushFail = true;
-							return;
-						}
-					}
-					level[i].p.x += player.d.x;
-					level[i].p.y += player.d.y;
-				}
-				break;
-		}
-	}
-	if (!pushFail) {
-		player.p.x += x; player.p.y += y;
 	}
 	draw();
 }
 
-draw();
+function nextLevelAlert() {
+	alert.style.left = '100vw';
+	if (currentLevel < levels.length - 1)
+		loadLevel(++currentLevel);
+}
+
+for (let [index, level] of levels.entries()) {
+	let button = document.createElement('button');
+	button.addEventListener('click', () => {
+		loadLevel(index);
+	});
+	button.innerHTML = index + 1;
+	levelSelect.appendChild(button)
+}
+
+function drawTile(im, { x: x, y: y }, { x: dx, y: dy } = {}) {
+	let img;
+	if (typeof dx !== 'undefined' && typeof dy !== 'undefined') {
+		img = document.getElementById(dirChar[dx] + dirChar[dy] + im);
+	} else {
+		img = document.getElementById(im);
+	}
+	ctx.drawImage(img, x * size, y * size, size, size)
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for (let pos of goals) {
+		drawTile('goal', pos);
+	}
+	for (let [i, tile] of level.entries()) {
+		switch (tile.n) {
+			case 'box':
+				drawTile('box', tile.p);
+				break;
+			case 'wall':
+				drawTile('wall', tile.p);
+				break;
+		}
+	}
+	drawTile('player', player.p, player.d);
+}
+
+let fail = false;
+
+function handleBox(index, tile) {
+	if (
+		player.p.x + player.d.x !== tile.p.x ||
+		player.p.y + player.d.y !== tile.p.y
+	) return;
+	for (let [indexTar, tileTar] of level.entries()) {
+		if (
+			(tileTar.n === 'box' || tileTar.n === 'wall') &&
+			(tile.p.x + player.d.x) === tileTar.p.x &&
+			(tile.p.y + player.d.y) === tileTar.p.y
+		) {
+			fail = true;
+			return;
+		}
+	}
+	level[index].p.x += player.d.x;
+	level[index].p.y += player.d.y;
+}
+
+function handleWall(tile) {
+	if (
+		tile.n === 'wall' &&
+		player.p.x + player.d.x === tile.p.x &&
+		player.p.y + player.d.y === tile.p.y
+	) {
+		fail = true;
+		return;
+	}
+}
+
+function isGoal(pos) {
+	for (let goal of goals) {
+		if (
+			pos.x === goal.x &&
+			pos.y === goal.y
+		) return true;
+	}
+	return false;
+}
+
+function update(x, y) {
+	player.d.x = x;
+	player.d.y = y;
+	let goalsReached = 0;
+	let boxes = {};
+	for (let [index, tile] of level.entries()) {
+		switch (tile.n) {
+			case 'box':
+				handleBox(index, tile);
+				if (isGoal(tile.p)) goalsReached++;
+				break;
+			case 'wall':
+				handleWall(tile);
+				break;
+		}
+	}
+	if (goalsReached === goals.length) {
+		console.log(
+			'%cyay !!!!',
+			'font-size: 54px; color: goldenrod; font-family: "Comic Sans MS", cursive; font-weight: bold;'
+		);
+		alert.style.left = '';
+	}
+	if (!fail) {
+		player.p.x += player.d.x;
+		player.p.y += player.d.y;
+	}
+	fail = false;
+	draw();
+}
+
+/*
+document.onkeydown = event => {
+	update(...({
+		'w': [0, -1],
+		's': [0, +1],
+		'a': [-1, 0],
+		'd': [+1, 0]
+	}[event.key]))
+}
+*/
+
+window.onload = () => {
+	alert.style.left = '100vw';
+	loadLevel(0);
+};
